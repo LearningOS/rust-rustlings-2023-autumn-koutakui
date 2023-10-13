5a90505594172e9dfb82e08469e66f1b9688d125
oslab <1586699654@qq.com>
2023-10-13 19:16:15 +0800

    update

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -11,7 +11,11 @@
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let mut v = vec![];// TODO: declare your vector here with the macro for vectors
+    
+    for n in 0..a.len() {
+        v.push(a[n]);
+    }
 
     (a, v)
 }
