e094c6d5bbc26e8466dc0d2e23b9c292ed8f60d8
oslab <1586699654@qq.com>
2023-10-23 08:25:53 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -5,21 +5,24 @@
 fn main() {
     // In tests7, we should set up an environment variable
     // called `TEST_FOO`. Print in the standard output to let
-    // Cargo do it
-    
+    // Cargo do it.
+    //println!("cargo：rustc-env=TEST_FOO=timestamp");
+    //cargo：rustc-env=VAR=VALUE
     let timestamp = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
         .as_secs(); // What's the use of this timestamp here?
     let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
+        "rustc-env=TEST_FOO={}",
         timestamp
     );
-    println!("cargo:{}={}", "TEST_FOO",your_command);
+    println!("cargo:{}", your_command);
+    
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
     let your_command = "Your command here, please checkout exercises/tests/build.rs";
     println!("cargo:{}", your_command);
+    //cargo：rustc-cfg=KEY[=“VALUE”]
 }
